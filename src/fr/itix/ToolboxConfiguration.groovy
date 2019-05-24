db9940871c9c9b4f2d8acf1f4d53ef67e51fb9d1
Nicolas Massé <nicolas.masse@itix.fr>
2019-05-24 16:30:03 +0200

    WiP

--- src/fr/itix/ToolboxConfiguration.groovy
+++ src/fr/itix/ToolboxConfiguration.groovy
@@ -3,7 +3,7 @@
 package fr.itix
 
 class ToolboxConfiguration {
-  def openshiftClient
+  String openshiftProject
   String destination
   String secretName
   String image = "quay.io/redhat/3scale-toolbox:v0.10.0"
@@ -20,153 +20,153 @@ class ToolboxConfiguration {
     assert conf.jobName != null
     assert conf.commandLine != null
 
-    // replace the global openshift variable of the OpenShift Client plugin by
-    // the one coming from the pipeline
-    def openshift = this.openshiftClient
+    openshift.withCluster() {
+      openshift.withProject(openshiftProject) {
+        def oasConfigMapName = null
+        if (conf.openAPI != null) {
+          oasConfigMapName = "3scale-toolbox-${JOB_BASE_NAME}-${BUILD_NUMBER}-openapi"
+          util.log "Creating a configMap named ${oasConfigMapName} containing the OpenAPI file..."
+          createConfigMap(openshift, oasConfigMapName, [ (conf.openAPI.filename): conf.openAPI.content ])
+        }
 
-    def oasConfigMapName = null
-    if (conf.openAPI != null) {
-      oasConfigMapName = "3scale-toolbox-${JOB_BASE_NAME}-${BUILD_NUMBER}-openapi"
-      util.log "Creating a configMap named ${oasConfigMapName} containing the OpenAPI file..."
-      createConfigMap(openshift, oasConfigMapName, [ (conf.openAPI.filename): conf.openAPI.content ])
-    }
-
-    def jobName = "${JOB_BASE_NAME}-${BUILD_NUMBER}-${conf.jobName}"
-    def jobSpecs = [
-      "apiVersion": "batch/v1",
-      "kind": "Job",
-      "metadata": [
-        "name": jobName,
-        "labels": [
-          "build": "${JOB_BASE_NAME}-${BUILD_NUMBER}",
-          "job": "${JOB_BASE_NAME}"
-        ]
-      ],
-      "spec": [
-        "backoffLimit": this.backoffLimit, 
-        "activeDeadlineSeconds": this.activeDeadlineSeconds,
-        "template": [
+        def jobName = "${JOB_BASE_NAME}-${BUILD_NUMBER}-${conf.jobName}"
+        def jobSpecs = [
+          "apiVersion": "batch/v1",
+          "kind": "Job",
+          "metadata": [
+            "name": jobName,
+            "labels": [
+              "build": "${JOB_BASE_NAME}-${BUILD_NUMBER}",
+              "job": "${JOB_BASE_NAME}"
+            ]
+          ],
           "spec": [
-            "restartPolicy": "Never",
-            "containers": [
-              [
-                "name": "job",
-                "image": this.image,
-                "imagePullPolicy": this.imagePullPolicy,
-                "command": [ "scl", "enable", "rh-ruby25", "/opt/rh/rh-ruby25/root/usr/local/bin/${conf.commandLine}" ],
-                "volumeMounts": [
+            "backoffLimit": this.backoffLimit, 
+            "activeDeadlineSeconds": this.activeDeadlineSeconds,
+            "template": [
+              "spec": [
+                "restartPolicy": "Never",
+                "containers": [
                   [
-                    "mountPath": "/opt/app-root/src/",
+                    "name": "job",
+                    "image": this.image,
+                    "imagePullPolicy": this.imagePullPolicy,
+                    "command": [ "scl", "enable", "rh-ruby25", "/opt/rh/rh-ruby25/root/usr/local/bin/${conf.commandLine}" ],
+                    "volumeMounts": [
+                      [
+                        "mountPath": "/opt/app-root/src/",
+                        "name": "toolbox-config"
+                      ],
+                      [
+                        "mountPath": "/artifacts",
+                        "name": "artifacts"
+                      ]
+
+                    ]
+                  ]
+                ],
+                "volumes": [
+                  [
                     "name": "toolbox-config"
                   ],
                   [
-                    "mountPath": "/artifacts",
                     "name": "artifacts"
                   ]
-
                 ]
-              ]
-            ],
-            "volumes": [
-              [
-                "name": "toolbox-config"
-              ],
-              [
-                "name": "artifacts"
               ]
             ]
           ]
         ]
-      ]
-    ]
 
-    // Inject the toolbox configuration as a volume
-    if (this.secretName != null) {
-      jobSpecs.spec.template.spec.volumes[0].secret = [
-        "secretName": this.secretName
-      ]
-    } else {
-      jobSpecs.spec.template.spec.volumes[0].emptyDir = [:]
-    }
-
-    // Inject the OpenAPI file as a volume
-    if (oasConfigMapName != null) {
-      jobSpecs.spec.template.spec.volumes[1].configMap = [
-        "name": oasConfigMapName
-      ]
-    } else {
-      jobSpecs.spec.template.spec.volumes[1].emptyDir = [:]
-    }
-
-    def job = null
-    try {
-      job = this.openshiftClient.create(jobSpecs)
-
-      int jobTimeout = 2 + (int)(this.activeDeadlineSeconds / 60.0f)
-      util.log "Waiting ${jobTimeout} minutes for the job to complete..."
-      timeout(jobTimeout) {
-        // Wait for the job to complete, either Succeeded or Failed
-        job.watch {
-          def jobStatus = getJobStatus(it.object())
-          util.log "Job ${it.name()}: succeeded = ${jobStatus.succeeded}, failed = ${jobStatus.failed}, status = ${jobStatus.status}, reason = ${jobStatus.reason}"
-          
-          // Exit the watch loop when the Job has one successful pod or failed
-          return jobStatus.succeeded > 0 || jobStatus.status == "Failed"
+        // Inject the toolbox configuration as a volume
+        if (this.secretName != null) {
+          jobSpecs.spec.template.spec.volumes[0].secret = [
+            "secretName": this.secretName
+          ]
+        } else {
+          jobSpecs.spec.template.spec.volumes[0].emptyDir = [:]
         }
-      }
-    } catch (e) {
-      util.log(e)
-    } finally {
-      // Delete the temporary configMap containing the OAS file
-      if (oasConfigMapName != null) {
+
+        // Inject the OpenAPI file as a volume
+        if (oasConfigMapName != null) {
+          jobSpecs.spec.template.spec.volumes[1].configMap = [
+            "name": oasConfigMapName
+          ]
+        } else {
+          jobSpecs.spec.template.spec.volumes[1].emptyDir = [:]
+        }
+
+        def job = null
         try {
-          openshift.selector('configMap', oasConfigMapName).delete()
-        } catch (e2) { // Best effort
-          util.log "cannot delete the configMap ${oasConfigMapName}: ${e2}"
-        }
-      }
+          job = this.openshiftClient.create(jobSpecs)
 
-      // If the job has been created, check its status
-      if (job != null) {
-        def jobStatus = getJobStatus(job.object())
-        util.log "job ${job.name()} has status '${jobStatus.status}' and reason '${jobStatus.reason}'"
-
-        // Iterate over pods to find:
-        //  - the pod that succeeded
-        //  - as last resort, a pod that failed 
-        def pods = job.related("pod")
-        pods.withEach {
-          if (it.object().status.phase == "Succeeded") {
-            result = getPodDetails(it)
+          int jobTimeout = 2 + (int)(this.activeDeadlineSeconds / 60.0f)
+          util.log "Waiting ${jobTimeout} minutes for the job to complete..."
+          timeout(jobTimeout) {
+            // Wait for the job to complete, either Succeeded or Failed
+            job.watch {
+              def jobStatus = getJobStatus(it.object())
+              util.log "Job ${it.name()}: succeeded = ${jobStatus.succeeded}, failed = ${jobStatus.failed}, status = ${jobStatus.status}, reason = ${jobStatus.reason}"
+              
+              // Exit the watch loop when the Job has one successful pod or failed
+              return jobStatus.succeeded > 0 || jobStatus.status == "Failed"
+            }
           }
-          if (it.object().status.phase == "Failed" && result == null) {
-            result = getPodDetails(it)
-          }
-        }
-
-        // Delete the job
-        try {
-          openshift.selector('job', jobName).delete()
-        } catch (e2) { // Best effort
-          util.log "cannot delete the job ${jobName}: ${e2}"
-        }
-
-        if (jobStatus.status != "Complete") {
-          // If there is at least a pod that failed, show its logs
-          if (result != null) {
-            util.log "RC: ${result.status}"
-            util.log "STDOUT:"
-            util.log "-------"
-            util.log result.stdout
-            util.log "STDERR:"
-            util.log "-------"
-            util.log result.stderr
+        } finally {
+          // Delete the temporary configMap containing the OAS file
+          if (oasConfigMapName != null) {
+            try {
+              openshift.selector('configMap', oasConfigMapName).delete()
+            } catch (e2) { // Best effort
+              util.log "cannot delete the configMap ${oasConfigMapName}: ${e2}"
+            }
           }
 
-          util.abort("job ${job.name()} exited with '${jobStatus.status}' and reason '${jobStatus.reason}'")
+          // If the job has been created, check its status
+          if (job != null) {
+            def jobStatus = getJobStatus(job.object())
+            util.log "job ${job.name()} has status '${jobStatus.status}' and reason '${jobStatus.reason}'"
+
+            // Iterate over pods to find:
+            //  - the pod that succeeded
+            //  - as last resort, a pod that failed 
+            def pods = job.related("pod")
+            pods.withEach {
+              if (it.object().status.phase == "Succeeded") {
+                result = getPodDetails(it)
+              }
+              if (it.object().status.phase == "Failed" && result == null) {
+                result = getPodDetails(it)
+              }
+            }
+
+            // Delete the job
+            try {
+              openshift.selector('job', jobName).delete()
+            } catch (e2) { // Best effort
+              util.log "cannot delete the job ${jobName}: ${e2}"
+            }
+
+            if (jobStatus.status != "Complete") {
+              // If there is at least a pod that failed, show its logs
+              if (result != null) {
+                util.log "RC: ${result.status}"
+                util.log "STDOUT:"
+                util.log "-------"
+                util.log result.stdout
+                util.log "STDERR:"
+                util.log "-------"
+                util.log result.stderr
+              }
+
+              util.abort("job ${job.name()} exited with '${jobStatus.status}' and reason '${jobStatus.reason}'")
+            }
+          }
         }
+            
       }
     }
+
 
     return result
   }
