4a2391e6c68e939e8f5a53260c315459a43f77b5
Nicolas Massé <nicolas.masse@itix.fr>
2019-05-24 15:42:42 +0200

    WiP

--- src/fr/itix/OpenAPI2.groovy
+++ src/fr/itix/OpenAPI2.groovy
@@ -14,20 +14,10 @@ class OpenAPI2 {
     }
 
     void parseOpenAPISpecificationFile() {
-        this.content = readOpenAPISpecificationFile(this.filename)
+        this.content = toolbox.readOpenAPISpecificationFile(this.filename)
         assert content.swagger == "2.0"
         this.version = content.info.version
         assert this.version != null
         this.majorVersion = version.tokenize(".")[0]
-    }
-}
-
-def readOpenAPISpecificationFile(fileName) {
-    if (fileName.toLowerCase().endsWith(".json")) {
-        return readJSON(file: fileName)
-    } else if (fileName.toLowerCase().endsWith(".yaml") || fileName.toLowerCase().endsWith(".yml")) {
-        return readYaml(file: fileName)
-    } else {
-        throw new Exception("Can't decide between JSON and YAML on ${fileName}")
     }
 }
