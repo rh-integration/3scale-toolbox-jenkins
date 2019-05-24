fef42704659c2738c5bc55049a0021821ee3f951
Nicolas Massé <nicolas.masse@itix.fr>
2019-05-24 16:03:39 +0200

    WiP

--- src/fr/itix/OpenAPI2.groovy
+++ src/fr/itix/OpenAPI2.groovy
@@ -14,7 +14,7 @@ class OpenAPI2 {
     }
 
     void parseOpenAPISpecificationFile() {
-        this.content = Util.readOpenAPISpecificationFile(this.filename)
+        this.content = new Util().readOpenAPISpecificationFile(this.filename)
         assert content.swagger == "2.0"
         this.version = content.info.version
         assert this.version != null
