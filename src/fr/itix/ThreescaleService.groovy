1fe8ec4ebc2e99e3911fea358c16d8def35dbfd1
Nicolas Massé <nicolas.masse@itix.fr>
2019-06-05 14:41:10 +0200

    work-in-progress

--- src/fr/itix/ThreescaleService.groovy
+++ src/fr/itix/ThreescaleService.groovy
@@ -29,7 +29,35 @@ class ThreescaleService {
                          "content": util.readFile(this.openapi.filename)
                        ])
   }
-  
+
+  void applyApplicationPlans() {
+    def globalOptions = toolbox.getGlobalToolboxOptions()
+    this.applicationPlans.each{
+      def commandLine = "3scale application-plan apply ${globalOptions} ${this.toolbox.destination} ${this.environment.targetSystemName} ${it.systemName} --approval-required=${it.approvalRequired} --cost-per-month=${it.costPerMonth} --end-user-required=${it.endUserRequired} --name=${it.name} --publish=${it.published} --setup-fee=${it.setupFee} --trial-period-days=${it.trialPeriodDays}"
+      if (it.defaultPlan) {
+        commandLine += " --default"
+      }
+
+      toolbox.runToolbox(commandLine: commandLine,
+                         jobName: "apply-application-plan-${it.systemName}")
+    }
+  }
+
+  void applyApplication(Map application) {
+    // See https://issues.jboss.org/browse/THREESCALE-2425
+    throw new Exception("NOT_IMPLEMENTED")
+  }
+
+  Map readProxy() {
+    // See https://issues.jboss.org/browse/THREESCALE-2405
+    throw new Exception("NOT_IMPLEMENTED")
+  }
+
+  void promoteToProduction() {
+    // See https://issues.jboss.org/browse/THREESCALE-2405
+    throw new Exception("NOT_IMPLEMENTED")
+  }
+
   static String basename(path) {
       def filename = path.drop(path.lastIndexOf("/") != -1 ? path.lastIndexOf("/") + 1 : 0)
       filename = filename.replaceAll("[^-._a-zA-Z0-9]", "_")
--- vars/toolbox.groovy
+++ vars/toolbox.groovy
@@ -12,12 +12,50 @@ ThreescaleService prepareThreescaleService(Map conf) {
   assert conf.environment != null
   assert conf.toolbox != null
   assert conf.service != null
+  assert conf.applicationPlans != null
+
+  List<ApplicationPlan> plans = []
+  conf.applicationPlans.each{
+    if (it.systemName == null || it.name == null) {
+      throw new Exception("Missing property in application plan: name or systemName")
+    }
+
+    ApplicationPlan plan = new ApplicationPlan(it)
+    plans.add(plan)
+  }
 
   OpenAPI2 openapi = new OpenAPI2(conf.openapi)
   openapi.parseOpenAPISpecificationFile()
   ThreescaleEnvironment environment = new ThreescaleEnvironment(conf.environment)
   ToolboxConfiguration toolbox = new ToolboxConfiguration(conf.toolbox + ["JOB_BASE_NAME": JOB_BASE_NAME, "BUILD_NUMBER": BUILD_NUMBER])
-  ThreescaleService service = new ThreescaleService([ "openapi": openapi, "environment": environment, "toolbox": toolbox ] + conf.service)
+  ThreescaleService service = new ThreescaleService([ "openapi": openapi, "environment": environment, "toolbox": toolbox, applicationPlans: plans ] + conf.service)
 
   return service
+}
+
+Map getDefaultApplicationCredentials(ThreescaleService service, String applicationName) {
+  String targetSystemName = service.environment.targetSystemName
+  String destination = service.toolbox.destination
+  String secretName = service.toolbox.secretName
+  String secret = ""
+  if (secretName != null) {
+    openshift.withCluster() {
+      openshift.withProject(service.toolbox.openshiftProject) {
+        def secretObject = openshift.selector('secret', secretName).object()
+        secret = secretObject.data[".3scalerc.yaml"]
+      }
+    }
+  }
+
+  writeFile file: "app_id", text: applicationName + targetSystemName + destination + secret
+  writeFile file: "app_secret", text: "secret" + applicationName + targetSystemName + destination + secret
+  def app_id = sha1 file: "app_id"
+  def app_secret = sha1 file: "app_secret"
+
+  // TODO: return user_key OR client_id/client_secret depending on the security scheme of the OpenAPI
+  return [
+    userKey: app_id,
+    clientId: app_id,
+    clientSecret: app_secret
+  ]
 }
