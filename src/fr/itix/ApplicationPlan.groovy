77bce33bb765da875670c5190e645842fb58496e
Nicolas MASSE <nicolas.masse@itix.fr>
2019-06-05 15:54:51 +0200

    Add method stubs + implement applyApplicationPlans (#2)
    * Add method stubs
    * implement applyApplicationPlans
    * re-indent

--- src/fr/itix/ApplicationPlan.groovy
+++ src/fr/itix/ApplicationPlan.groovy
@@ -5,4 +5,11 @@ package fr.itix
 class ApplicationPlan {
     String systemName
     String name
+    boolean approvalRequired = false
+    boolean defaultPlan = false
+    boolean endUserRequired = false
+    boolean published = false
+    BigDecimal costPerMonth = 0.0
+    BigDecimal setupFee = 0.0
+    BigInteger trialPeriodDays = 0
 }
