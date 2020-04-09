3f5f9eea6ca91fe4285ea760c00da75cff623707
Nicolas Massé <nicolas.masse@itix.fr>
2020-04-09 17:45:40 +0200

    iface -> abstract class

--- src/com/redhat/OpenAPI3.groovy
+++ src/com/redhat/OpenAPI3.groovy
@@ -2,39 +2,9 @@
 
 package com.redhat
 
-class OpenAPI3 implements OpenAPI {
-    String filename
-    def content
-    String version
-    String majorVersion
-    ThreescaleSecurityScheme securityScheme
-    boolean validateOAS = true
-
+class OpenAPI3 extends OpenAPI {
     OpenAPI3(Map conf) {
-        assert conf.filename != null
-        assert conf.content != null
-        this.filename = conf.filename
-        this.content = conf.content
-    }
-
-    String getUpdatedContent() {
-        Util util = new Util()
-        String baseName = "updated-" + util.basename(this.filename)
-        util.removeFile(baseName)
-        util.writeOpenAPISpecificationFile(baseName, this.content)
-        return util.readFile(baseName)
-    }
-
-    void updateTitleWithEnvironmentAndVersion(String environmentName) {
-        String title = this.content.info.title as String
-        String version = this.version as String
-        String newTitle = null
-        if (environmentName != null) {
-            newTitle = "${title} (${environmentName.toUpperCase()}, v${version})"
-        } else {
-            newTitle = "${title} (v${version})"
-        }
-        this.content.info.title = newTitle
+        super(conf);
     }
 
     void parseOpenAPISpecificationFile() {
